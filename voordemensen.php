7fae60a1dfe5ad549b63143fefad07f845275ec8
Jeroen voordeMensen <jeroen@voordemensen.nl>
2020-07-06 13:33:50 +0200

    bugfix

--- voordemensen.php
+++ voordemensen.php
@@ -3,7 +3,7 @@
  * Plugin Name:       VoordeMensen
  * Plugin URI:        https://voordemensen.nl
  * Description:       Verbind WordPress met het VoordeMensen kaartverkoopsysteem
- * Version:           1.0.4
+ * Version:           1.0.6
  * Author:            VoordeMensen
  * Author URI:        https://voordemensen.nl
  * License:           GPL v2 or later
@@ -23,9 +23,9 @@ function vdm_load_loader() {
 	$vdm_client_shortname = wp_strip_all_tags(get_option('vdm_client_shortname'));
 	$vdm_options = get_option('vdm_options');
 	if($vdm_options['vdm_loader_type']=='side') {
-		wp_enqueue_script('vdm_loader','http://tickets.test/'.$vdm_client_shortname.'/iframes/vdm_sideloader.js');
+		wp_enqueue_script('vdm_loader','https://tickets.voordemensen.nl/'.$vdm_client_shortname.'/iframes/vdm_sideloader.js');
 	} else {
-		wp_enqueue_script('vdm_loader','http://tickets.test/'.$vdm_client_shortname.'/iframes/vdm_loader.js');	
+		wp_enqueue_script('vdm_loader','https://tickets.voordemensen.nl/'.$vdm_client_shortname.'/iframes/vdm_loader.js');	
 	}
 }
 
